996d4d17bd02eea197aba615fbbdef538e29354d
Chakrit <chakrit.t@arise.tech>
2025-04-12 16:25:32 +0700

    hexagonal: handle main

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"github.com/NewChakrit/golang_hexagonal/adapters"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 
+	"github.com/NewChakrit/golang_hexagonal/core"
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	// initialize the database connection
+	// filed based
+	db, err := gorm.Open(sqlite.Open("orders.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	orderRepo := adapters.NewGormOrderRepository(db)
+	orderService := core.NewOrderService(orderRepo)
+	orderHandler := adapters.NewHttpOrderHandler(orderService)
+
+	app.Post("/order", orderHandler.CreateOrder)
+	// migrate the schema
+	db.AutoMigrate(&core.Order{})
+
+	app.Listen(":8080")
 }
